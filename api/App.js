cbf58088aa40f072814d3267ae8fcb99613ae878
AlexV105 <150324510+AlexV105@users.noreply.github.com>
2024-03-05 00:55:23 +0200

    Add files via upload

---
+++ api/App.js
@@ -0,0 +1,23 @@
+<table className="table">
+    <thread>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">First name</th>
+            <th scope="col">Last name</th>
+            <th scope="col">Actions</th>
+        </tr>
+    </thread>
+    <tbody>
+
+
+        <tr >
+            <th scope="row">1</th>
+            <td>John</td>
+            <td>Doe</td>
+            <td>
+                <i className="fa-solid fa-pen-to-square " style={{ color: "green", cursor: "pointer", marginRight: "25px" }}></i>
+                <i style={{ color: "red", cursor: "pointer"}} className="fa-solid fa-trash-can"></i>
+            </td>
+        </tr>
+        </tbody>
+        </table>
